3d6fcc86b4b5582f81124838dc37fe1e8fbb68a6
Huynh Minh Bao <huynhminhbao2808@gmail.com>
2021-06-06 22:52:27 +0700

    Update Project

---
+++ Assets/Scripts/GameManger.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManger : UIGamePlayController {
+    public static GameManger instance;
+    //[SerializeField] private Text scoreText;
+    //[SerializeField] private Text coinText;
+    [SerializeField] private GameObject gameOverText;
+    [SerializeField] private Text scoreInGameText;
+    [SerializeField] private Text coinInGameText;
+    //[SerializeField] private Text yourScoreText;
+    //[SerializeField] private Text yourHighScoreText;
+    private int bestScore;
+    private int gameScore;
+    private int coinPoint;
+    static int timeToPlay;
+    private bool gameOver =  false;
+
+
+    private void Awake() {
+        if (instance == null) {
+            instance = this;
+        }
+    }
+
+    private void Start() {
+        coinPoint = PlayerPrefs.GetInt("coinPoint", coinPoint);
+        bestScore = PlayerPrefs.GetInt("bestScore", bestScore);
+        coinInGameText.text = "" + coinPoint;
+        //SetCoin(coinPoint);
+    }
+
+    public bool GameOver {
+        get { return gameOver; }
+    }
+
+    public int GameScore {
+        get { return gameScore; }
+    }
+
+    public void calculateScore(int score) {
+        gameScore += score;
+        if (gameScore > bestScore) {
+			bestScore = gameScore;
+			PlayerPrefs.SetInt("bestScore", bestScore);
+		}
+        scoreInGameText.text = "" + gameScore;
+        //SetPoint(gameScore);
+    }
+
+    public void calculatorCoin(int coin) {
+        coinPoint += coin;
+        coinInGameText.text = "" + coinPoint;
+        PlayerPrefs.SetInt("coinPoint", coinPoint);
+    }
+
+    public void tankGet0 () {
+        gameOver = true;
+        timeToPlay++;
+        gameOverText.SetActive(true);
+        //yourScoreText.text = "" + gameScore;
+        //yourHighScoreText.text = "" + bestScore;
+        SetYourScore(gameScore);
+        SetYourBestScore(bestScore);
+        SetYourEnemyKill(EventGameManager.instance.EnemyKillCount);
+        SetCoin(coinPoint);
+    }
+
+    public void GotoGameMenu() {
+        GotoScene("GamePlay");
+    }
+
+    public void GotoMenu() {
+        GotoScene("GameMenu");
+    }
+}
---
+++ Assets/Scripts/MenuGameMangager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuGameMangager : MonoBehaviour {
+    public static MenuGameMangager instance = null;
+	private int bestScore;
+
+    private void Start() {
+        Invoke("CallAdsBanner", 0.01f);
+        bestScore = PlayerPrefs.GetInt("bestScore", bestScore);
+    }
+
+	private void OnHideUnity(bool isGameShown)
+	{
+		if (!isGameShown)
+		{
+			// Pause the game - we will need to hide
+			Time.timeScale = 0;
+		}
+		else
+		{
+			// Resume the game - we're getting focus again
+			Time.timeScale = 1;
+		}
+	}
+
+	public void GoToGamePlay() {
+        SceneManager.LoadScene("GamePlay");
+    }
+
+    public void GoToShop() {
+        SceneManager.LoadScene("Shop");
+    }
+}
